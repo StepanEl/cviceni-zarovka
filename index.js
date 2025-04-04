448acd36d02897839fcef7e1d7073660b1701e79
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-04-04 15:26:34 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('funguju!');
+
+const bulb = document.querySelector('.bulb');
+
+const toggleBulb = () => {
+    bulb.classList.toggle('bulb--on');
+};
+document.addEventListener('keydown', toggleBulb);
+
+
+/*kratší verze:;
+document.addEventListener('keydown', () => {
+    bulb.classList.toggle('bulb--on');
+    });
